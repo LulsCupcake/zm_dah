9f03d98f3f89eab3d24d93d609b84e03c9221d38
Sora <timertimer5@gmail.com>
2019-04-28 20:33:09 +0200

    Skylib

--- zm_instances/hooks/Setup.lua
+++ zm_instances/hooks/Setup.lua
@@ -1,7 +1,2 @@
-dofile(ModPath .. "classes/WDUManager.lua")
-dofile(ModPath .. "classes/WDUPowerUps.lua")
-
-Hooks:PostHook(GameSetup, "init_managers", "zm_init_wdumanager", function(self, managers)
-	managers.wdu = WDUManager:new()
-	managers.wdu:preload_weapons()
-end)
+SkyLib:set_mod_key(zm_testing)
+SkyLib:init_by_gamemode("codzm")
