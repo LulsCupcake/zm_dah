1de4a37e862f2acb056683546bf1d17f2f96b498
Cupcake <meowcats2121@gmail.com>
2020-10-29 18:33:52 -0600

    added some small EEs

---
+++ Garnet Group Tower/hooks/tweakdata.lua
@@ -0,0 +1,15 @@
+	tweak_data.overlay_effects.jumpscare = {
+			text_to_upper = true,
+			sustain = 0.5,
+			localize = true,
+			fade_in = 0.25,
+			text_blend_mode = "add",
+			fade_out = 0.25,
+			font = "fonts/font_large_mf",
+			text = "boo",
+			font_size = 150,
+			blend_mode = "normal",
+			color = Color(1, 0, 0, 0),
+			timer = TimerManager:main(),
+			text_color = Color(255, 255, 204, 0) / 255
+		}
